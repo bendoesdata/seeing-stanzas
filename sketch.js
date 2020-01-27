c1f9822081f545647b4de6e70f8e0500b977568c
bendoesdata <benjamincooley94@gmail.com>
2020-01-27 17:22:41 -0500

    trying pos tagging

--- sketch.js
+++ sketch.js
@@ -9,11 +9,14 @@ function preload() {
 function setup() {
     createCanvas(800, 800);
 
-    input = createElement('textarea', 'Who is your favorite cat?');
+    input = createElement('textarea', "The car is wearing a red hat from Alabama. I am a red banana. Hello!")
     input.size(200, 100);
     input.position(500, 20)
 
-    console.log(typeof input.value())
+    ;
+
+    console.log(RiTa.getPosTags(input.value()))
+
 
 }
 
@@ -25,23 +28,38 @@ function draw() {
 
     fill(20);
     poem = input.value();
+    var tags = RiTa.getPosTags(input.value(), true);
     poemLength = poem.split("\n");
     for (var i = 0; i < poemLength.length; i++) {
         var words = poemLength[i].split(" ");
         var currentOffset = 0;
-
+        let tag = "";
         for (var j = 0; j < words.length; j++) {
             var wordWidth = textWidth(words[j]);
-            fill(30, 150);
+            tag = tags[j];
+            if (tag == "n") {
+                fill("#72b5b7")
+            } else if (tag == "a") {
+                fill("#633a82");
+            } else if (tag == "v") {
+                fill('#40e0d0')
+            } else if (tag == "r") {
+                fill(250)
+            } else {
+                fill(30, 150);
+            }
+
             // rect(25 + currentOffset, 25 + i * 20,
             //     wordWidth, 6);
 
-            ellipse(20 + currentOffset, 20 + i * 20, wordWidth * 0.6);
+            ellipse(20 + currentOffset, 20 + i * 40, wordWidth * 0.6);
 
-            if (mouseIsPressed) {
-                fill(0);
-                text(words[j], 25 + currentOffset, 25 + i * 20);
-            }
+            // show words if mouse is pressed
+            // if (mouseIsPressed) {
+            //     fill(0);
+            //     text(words[j], 25 + currentOffset, 25 + i * 20);
+            // }
+
             // four pixels between words
             currentOffset += wordWidth + 4;
         }
